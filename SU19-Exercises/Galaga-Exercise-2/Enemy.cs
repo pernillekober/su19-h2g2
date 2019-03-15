1053a6430e56e19f33128d7327b76116a03029d8
pernillekober <pernillekoeop@gmail.com>
2019-03-15 11:24:15 +0100

    Enemy

--- SU19-Exercises/Galaga-Exercise-2/Enemy.cs
+++ SU19-Exercises/Galaga-Exercise-2/Enemy.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
+using DIKUArcade.Math;
 
 // mangler namespace
-namespace Galaga_Exercise_2 {
+namespace Galaga_Exercise_2.GalagaEntities.Enemy {
     public class Enemy : Entity {
         private Game game;
+        private Vec2F vec2F { get; }
 
         public Enemy(Game game, DynamicShape shape, IBaseImage image) :
             base(shape, image) {
