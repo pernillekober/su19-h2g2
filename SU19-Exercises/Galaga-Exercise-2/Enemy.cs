9839b08a2391c5df71cf5b8d94d1a85bebdbb154
pernillekober <pernillekoeop@gmail.com>
2019-03-15 11:37:07 +0100

    Enemy pos

--- SU19-Exercises/Galaga-Exercise-2/Enemy.cs
+++ SU19-Exercises/Galaga-Exercise-2/Enemy.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
 using DIKUArcade.Math;
+using OpenTK.Input;
 
 // mangler namespace
 namespace Galaga_Exercise_2.GalagaEntities.Enemy {
@@ -12,6 +13,8 @@ namespace Galaga_Exercise_2.GalagaEntities.Enemy {
         public Enemy(Game game, DynamicShape shape, IBaseImage image) :
             base(shape, image) {
             this.game = game;
+            Vec2F position = new Vec2F(0.0f,0.0f); 
+            
         }
     }
 }
